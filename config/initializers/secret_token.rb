bd2a1c97a3093ea492202d4cd1ae01c21a4aa57d
Kyle Gann <kylegann@gmail.com>
2012-04-20 23:47:10 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wtl::Application.config.secret_token = '54c298f67f1f44f4412ff428efd198a590a3fc96a761984c8946547a1735d5f50665da546704ca28898ca6266cc042ce27f6b3ff095d33b6149f7b96691afa24'
